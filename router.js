6c435dc8e946fe59229270521571cc79d65038f7
ivan <ivnbenitez@gmail.com>
2020-11-18 22:19:03 -0300

    archivos

--- router.js
+++ router.js
@@ -5,6 +5,26 @@ module.exports = function(req,res) {
         case '/':
             index.homePage(req,res)
             break;
+        case "/ en-cartelera" :
+        index.enCartelera(req,res)
+        break;
+        case "/contacto":
+            index.contacto(req,res)
+        break;
+        case "/mas-Votadas":
+        index.masVotadas(req,res)
+        break;
+        case "/preguntas-Frecuentes":
+            index.preguntasFrecuentes(req,res);
+            break;
+            case "/sucursales":
+                index.sucursales(req,res)
+                break;
+                default:
+                    res.write("Pagina No Disponible")
+                    break;
+
+
     }
 
 }
