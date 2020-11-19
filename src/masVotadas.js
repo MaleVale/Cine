3cd6379bba561e3fc43f8124499bd09b4929bf00
Alexus167 <alexivanmendez08@gmail.com>
2020-11-19 17:38:13 -0300

    masVotadas

--- src/masVotadas.js
+++ src/masVotadas.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
-
-module.exports = {
+let masVotadas = {
     leerJSON : function(){
         const fs = require("fs");
         return JSON.parse(fs.readFileSync('./data/movies.json','utf-8'));
+        let votePromedioSiete = movie.filter((movies)=>{return movies.vote_average >= 7});	  
+            peliculasConPromedioSiete = voteSiete.length;
+            peliculasConDescipcion = movies.overview;
+		
+    }
 }
-}
+
+module.exports = masVotadas
